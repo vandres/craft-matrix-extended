e2ca34d90980ad0ed4beb8f3498f0dd1e0f154e9
Volker Andres <andres@voan.ch>
2024-11-26 17:19:48 +0100

    feat(duplicate): first working version for cards - unfortunately a lot of JS code duplication - creating a draft is not working yet. Duplication always creates a published version

---
+++ src/controllers/NestedElementExtendedController.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace vandres\matrixextended\controllers;
+
+use Craft;
+use craft\base\Element;
+use craft\base\ElementInterface;
+use craft\base\NestedElementInterface;
+use craft\elements\db\AssetQuery;
+use craft\elements\db\ElementQueryInterface;
+use craft\elements\db\EntryQuery;
+use craft\elements\Entry;
+use craft\fields\Matrix;
+use craft\helpers\ElementHelper;
+use craft\helpers\StringHelper;
+use vandres\matrixextended\MatrixExtended;
+use vandres\matrixextended\models\Settings;
+use yii\web\BadRequestHttpException;
+use yii\web\ForbiddenHttpException;
+use yii\web\Response;
+
+class NestedElementExtendedController extends \craft\web\Controller
+{
+    private ?Settings $settings;
+
+    public function beforeAction($action): bool
+    {
+        if (!parent::beforeAction($action)) {
+            return false;
+        }
+
+        $this->requireCpRequest();
+
+        $this->settings = MatrixExtended::getInstance()->getSettings();
+        return true;
+    }
+
+    /**
+     * Creates a new entry with the content of another
+     *
+     * @return Response
+     */
+    public function actionDuplicateEntry(): Response
+    {
+        if (!$this->settings->experimentalFeatures) {
+            throw new ForbiddenHttpException('Experimental features not enabled.');
+        }
+
+        $entryId = $this->request->getRequiredBodyParam('id');
+        $fieldId = $this->request->getRequiredBodyParam('fieldId');
+        $ownerId = $this->request->getRequiredBodyParam('ownerId');
+        $siteId = $this->request->getRequiredBodyParam('siteId');
+
+        $elementsService = Craft::$app->getElements();
+        $entry = $elementsService->getElementById($entryId, 'craft\elements\Entry', $siteId);
+        if (!$entry) {
+            throw new BadRequestHttpException("Invalid entry ID, element type, or site ID.");
+        }
+        $owner = $elementsService->getElementById($ownerId, 'craft\elements\Entry', $siteId);
+        if (!$owner) {
+            throw new BadRequestHttpException("Invalid owner ID, element type, or site ID.");
+        }
+
+        $field = $owner->getFieldLayout()?->getFieldById($fieldId);
+        if (!$field instanceof Matrix) {
+            throw new BadRequestHttpException("Invalid Matrix field ID: $fieldId");
+        }
+
+        $site = Craft::$app->getSites()->getSiteById($siteId, true);
+        if (!$site) {
+            throw new BadRequestHttpException("Invalid site ID: $siteId");
+        }
+
+        $user = static::currentUser();
+        if (!$entry->canSave($user)) {
+            throw new ForbiddenHttpException('User not authorized to duplicate this element.');
+        }
+
+
+//        ray($entry);
+//        ray($duplicatedEntry->sortOrder);
+
+        $attribute = 'field:dyncontent';
+        $nestedElements = $owner->$attribute;
+
+        if ($nestedElements instanceof ElementQueryInterface) {
+            $oldSortOrders = (clone $nestedElements)
+                ->status(null)
+                ->asArray()
+                ->select(['id', 'sortOrder'])
+                ->pairs();
+        } else {
+            $oldSortOrders = $nestedElements
+                ->keyBy(fn(ElementInterface $element) => $element->id)
+                /** @phpstan-ignore-next-line */
+                ->map(fn(NestedElementInterface $element) => $element->getSortOrder())
+                ->all();
+        }
+
+        ray($oldSortOrders);
+        $entry->sortOrder = array_key_exists($entry->id, $oldSortOrders) ? $oldSortOrders[$entry->id] : 0;
+
+
+        $duplicatedEntry = $this->cloneEntry($entry, $ownerId, $siteId);
+
+
+//        ray($entry);
+        ray($duplicatedEntry);
+//
+//        ray($entry);
+//
+//        $structuresService = Craft::$app->getStructures();
+//        $structuresService->moveAfter($entry->structureId, $duplicatedEntry, $entry);
+
+        return $this->asJson($duplicatedEntry);
+    }
+
+    private function cloneEntry(Entry $entry, $ownerId, $siteId = null)
+    {
+        $elementsService = Craft::$app->getElements();
+
+        // With Craft 5.5.x, the native Duplication started working
+        if (version_compare(\Craft::$app->getVersion(), '5.5.0', '>=')) {
+            return $elementsService->duplicateElement($entry);
+        }
+
+        $owner = $elementsService->getElementById($ownerId, 'craft\elements\Entry', $siteId);
+
+        // Ensure all fields have been normalized
+        $entry->getFieldValues();
+
+        $duplicatedEntry = Craft::createObject([
+            'class' => Entry::class,
+            'siteId' => $entry->siteId,
+            'uid' => StringHelper::UUID(),
+            'typeId' => $entry->typeId,
+            'fieldId' => $entry->fieldId,
+            'owner' => $owner,
+            'title' => $entry->title,
+            'slug' => ElementHelper::tempSlug(),
+            'fieldValues' => $entry->getFieldValues(),
+        ]);
+        $duplicatedEntry->setScenario(Element::SCENARIO_ESSENTIALS);
+        $children = [];
+        $transaction = Craft::$app->getDb()->beginTransaction();
+
+        try {
+            $dirtyFields = $duplicatedEntry->getDirtyFields();
+            $saveLater = [];
+            foreach ($entry->getFieldValues() as $handle => $value) {
+                if ($value instanceof AssetQuery) {
+                    $saveLater[] = [
+                        'handle' => $handle,
+                        'value' => $value,
+                    ];
+                } else if ($value instanceof EntryQuery) {
+                    $elements = $value->status(null)->all();
+                    $matrixElements = array_filter($elements, function ($element) {
+                        return !!$element->fieldId;
+                    });
+
+                    if (empty($matrixElements)) {
+                        $saveLater[] = [
+                            'handle' => $handle,
+                            'value' => $value,
+                        ];
+                    } else {
+                        $children[] = ['owner' => $entry->id, 'handle' => $handle, 'elements' => $matrixElements];
+                    }
+                } else if (is_object($value) && !$value instanceof \UnitEnum) {
+                    $duplicatedEntry->setFieldValue($handle, clone $value);
+                }
+            }
+            $duplicatedEntry->setDirtyFields($dirtyFields, false);
+            if (!$elementsService->saveElement($duplicatedEntry, false)) {
+                throw new \Exception('Could not save element on cloning process');
+            }
+
+            foreach ($children as $child) {
+                foreach ($child['elements'] as $childElement) {
+                    $this->cloneEntry($childElement, $duplicatedEntry->id, $siteId);
+                }
+            }
+
+            if (!empty($saveLater)) {
+                foreach ($saveLater as $field) {
+                    $duplicatedEntry->setFieldValue($field['handle'], clone $field['value']);
+                }
+
+                if (!$elementsService->saveElement($duplicatedEntry, false)) {
+                    throw new \Exception('Could not save element on cloning process');
+                }
+            }
+
+            $transaction->commit();
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+
+        return $duplicatedEntry;
+    }
+}
--- static/src/js/nestedElementExtended.ts
+++ static/src/js/nestedElementExtended.ts
@@ -24,31 +24,20 @@
             const nestedInitFn = Craft.NestedElementManager.prototype.init;
             Craft.NestedElementManager.prototype.init = function (...args: any[]) {
                 nestedInitFn.apply(this, args);
-                if (!self.settings.experimentalFeatures) {
-                    return;
-                }
+            };
 
-                const parent = this;
-                if (parent.settings.mode !== 'cards' || !parent.settings.canCreate) {
-                    return;
-                }
-
-                console.log('NestedElementManager');
-
-                console.log(parent.settings);
-                console.log(self.settings);
-
-                const disclosureMenuShowFn = Garnish.DisclosureMenu.prototype.show;
-                Garnish.DisclosureMenu.prototype.show = function (...args: any[]) {
-                    self.initDisclosureMenu(this);
-                    disclosureMenuShowFn.apply(this, args);
-                };
-
+            const disclosureMenuShowFn = Garnish.DisclosureMenu.prototype.show;
+            Garnish.DisclosureMenu.prototype.show = function (...args: any[]) {
+                self.initDisclosureMenu(this);
+                disclosureMenuShowFn.apply(this, args);
             };
         },
 
         initDisclosureMenu(disclosureMenu: any) {
-            console.log('initDisclosureMenu');
+            if (!this.settings.experimentalFeatures) {
+                return;
+            }
+
             const {$trigger, $container} = disclosureMenu;
             if (!$trigger || !$container || !$trigger.hasClass('action-btn')) {
                 return;
@@ -64,7 +53,156 @@
                 return;
             }
 
-            console.log($element.data());
+            const $parent = $element.closest('.nested-element-cards');
+            const nem = $parent.data('nestedElementManager');
+            if (!nem) {
+                return;
+            }
+
+            if (nem.settings.mode !== 'cards' || !nem.settings.canCreate) {
+                return;
+            }
+
+            if (disclosureMenu._hasNestedElementExtensionButtonsInitialized) {
+                // this.checkPaste($container, matrix);
+                this.checkDuplicate($container, nem);
+                // this.checkAdd($container, matrix);
+                return;
+            }
+            disclosureMenu._hasNestedElementExtensionButtonsInitialized = true;
+
+            this.addMenu($container, typeId, $element, nem);
         },
+
+        addMenu: function ($container: any, typeId: any, $element: any, nem: any) {
+            const $menu = $('<ul class="matrix-extended"></ul>');
+            const $hr = $('<hr class="padded">');
+
+            // this.addAddBlockButton($menu, typeId, entry, matrix);
+            this.addDuplicateButton($container, $menu, typeId, $element, nem);
+            // this.addCopyButton($menu, typeId, entry, matrix);
+            // this.addPasteButton($menu, typeId, entry, matrix);
+            // this.addDeleteButton($menu, entry);
+            // this.checkPaste($menu, matrix);
+            this.checkDuplicate($container, nem);
+            // this.checkAdd($menu, matrix);
+
+            $menu.insertBefore($container.find('ul').eq(0));
+            $hr.insertAfter($menu);
+        },
+
+        addDuplicateButton: function ($container: any, $menu: any, typeId: any, $element: any, nem: any) {
+            const $duplicateButton = $(`<li>
+                <button class="menu-item" data-action="duplicate" tabindex="0">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 464H288c8.8 0 16-7.2 16-16V384h48v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h64v48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16zM224 304H448c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H224c-8.8 0-16 7.2-16 16V288c0 8.8 7.2 16 16 16zm-64-16V64c0-35.3 28.7-64 64-64H448c35.3 0 64 28.7 64 64V288c0 35.3-28.7 64-64 64H224c-35.3 0-64-28.7-64-64z"/></svg>
+                    </span><span class="menu-item-label">${Craft.t('matrix-extended', 'Duplicate')}</span>
+                </button>
+            </li>`);
+            $menu.append($duplicateButton);
+
+            $duplicateButton.find('button').on('click', () => {
+                this.duplicateEntry($container, $menu, typeId, $element, nem);
+            });
+        },
+
+        duplicateEntry: async function ($container: any, $menu: any, typeId: any, $element: any, nem: any) {
+            if (!nem.canCreate()) {
+                return;
+            }
+
+            // await nem.markAsDirty();
+
+            try {
+                const {data} = await Craft.sendActionRequest('POST', 'matrix-extended/nested-element-extended/duplicate-entry', {
+                    data: $element.data(),
+                });
+
+                await this.addElementCard(data, nem, $element.data('id'));
+            } catch (error) {
+                this.addStatusMessage(Craft.t('matrix-extended', 'There was an error duplicating the entry'), 'error');
+            }
+
+            $container.data('disclosureMenu').hide();
+        },
+
+        checkDuplicate: function ($container: any, nem: any) {
+            const $duplicateButton = $container.find('button[data-action="duplicate"]');
+            $duplicateButton.disable();
+            const $parent = $duplicateButton.parent();
+            $parent.attr('title', '');
+
+            if (!nem.canCreate()) {
+                $parent.attr('title', Craft.t('matrix-extended', 'No more entries can be added.'));
+                return;
+            }
+
+            $duplicateButton.enable();
+        },
+
+        async addElementCard(element: any, nem: any, insertAfter: number) {
+            if (nem.$createBtn) {
+                nem.$createBtn.addClass('loading');
+            }
+
+            let response;
+            try {
+                response = await Craft.sendActionRequest(
+                    'POST',
+                    'app/render-elements',
+                    {
+                        data: {
+                            elements: [
+                                {
+                                    type: nem.elementType,
+                                    id: element.id,
+                                    siteId: element.siteId,
+                                    instances: [
+                                        {
+                                            context: 'field',
+                                            ui: 'card',
+                                            sortable: nem.settings.sortable,
+                                            showActionMenu: true,
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    }
+                );
+            } catch (e: any) {
+                Craft.cp.displayError(e?.response?.data?.message);
+                throw e?.response?.data?.message ?? e;
+            } finally {
+                if (nem.$createBtn) {
+                    nem.$createBtn.removeClass('loading');
+                }
+            }
+
+            if (!nem.$elements) {
+                nem.initCards();
+            }
+
+            const $li = $('<li/>');
+            const $card = $(response.data.elements[element.id][0]).appendTo($li);
+            nem.$elements.find(`[data-id="${insertAfter}"`).parent().after($li);
+            nem.initElement($card);
+            await Craft.appendHeadHtml(response.data.headHtml);
+            await Craft.appendBodyHtml(response.data.bodyHtml);
+            Craft.cp.elementThumbLoader.load($card);
+            nem.updateCreateBtn();
+
+            return $card;
+        },
+
+        addStatusMessage: function (message: string, type: 'notice' | 'error' = 'notice') {
+            if (type === 'notice') {
+                Craft.cp.displayNotice(message);
+            }
+
+            if (type === 'error') {
+                Craft.cp.displayError(message);
+            }
+        }
     });
 })(window);
