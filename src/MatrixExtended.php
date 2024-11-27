da414117887738f4b0883d9201e3870c31cbb8ae
Volker Andres <andres@voan.ch>
2024-11-27 19:10:47 +0100

    fix(global): take care, that "type-id" is on each card (cards)

--- src/MatrixExtended.php
+++ src/MatrixExtended.php
@@ -5,9 +5,13 @@ namespace vandres\matrixextended;
 use Craft;
 use craft\base\Model;
 use craft\base\Plugin;
+use craft\events\DefineElementHtmlEvent;
+use craft\helpers\Cp;
+use craft\helpers\Html;
 use vandres\matrixextended\models\Settings;
 use vandres\matrixextended\services\MatrixService;
 use vandres\matrixextended\web\assets\cp\MatrixExtendedAsset;
+use yii\base\Event;
 
 /**
  * Matrix Extended plugin
@@ -41,27 +45,6 @@ class MatrixExtended extends Plugin
         $this->setUpCp();
     }
 
-    protected function createSettingsModel(): ?Model
-    {
-        return Craft::createObject(Settings::class);
-    }
-
-    protected function settingsHtml(): ?string
-    {
-        // Get and pre-validate the settings
-        $settings = $this->getSettings();
-        $settings->validate();
-
-        // Get the settings that are being defined by the config file
-        $overrides = Craft::$app->getConfig()->getConfigFromFile(strtolower($this->handle));
-
-        return Craft::$app->view->renderTemplate('matrix-extended/_settings.twig', [
-            'plugin' => $this,
-            'overrides' => array_keys($overrides),
-            'settings' => $settings,
-        ]);
-    }
-
     private function setUp()
     {
         Craft::$app->onInit(function () {
@@ -85,6 +68,50 @@ class MatrixExtended extends Plugin
             }
 
             \Craft::$app->getView()->registerAssetBundle(MatrixExtendedAsset::class);
+
+            // Make sure element cards and chips have a [data-type-id] attribute, which we'll use to bootstrap an "Edit Entry Type" link in their settings menus
+            foreach ([Cp::EVENT_DEFINE_ELEMENT_CARD_HTML, Cp::EVENT_DEFINE_ELEMENT_CHIP_HTML] as $eventName) {
+                Event::on(
+                    Cp::class,
+                    $eventName,
+                    static function (DefineElementHtmlEvent $event) {
+                        $typeId = $event->element?->typeId ?? null;
+                        if (empty($typeId)) {
+                            return;
+                        }
+                        try {
+                            $html = Html::modifyTagAttributes($event->html, [
+                                'data-type-id' => $typeId,
+                            ]);
+                        } catch (\Throwable $e) {
+                            Craft::error($e, __METHOD__);
+                            return;
+                        }
+                        $event->html = $html;
+                    }
+                );
+            }
         });
+    }
+
+    protected function createSettingsModel(): ?Model
+    {
+        return Craft::createObject(Settings::class);
+    }
+
+    protected function settingsHtml(): ?string
+    {
+        // Get and pre-validate the settings
+        $settings = $this->getSettings();
+        $settings->validate();
+
+        // Get the settings that are being defined by the config file
+        $overrides = Craft::$app->getConfig()->getConfigFromFile(strtolower($this->handle));
+
+        return Craft::$app->view->renderTemplate('matrix-extended/_settings.twig', [
+            'plugin' => $this,
+            'overrides' => array_keys($overrides),
+            'settings' => $settings,
+        ]);
     }
 }
